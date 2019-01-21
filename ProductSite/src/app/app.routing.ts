bdedba6ac9ab4bad2f9afc3d6454dc303e396760
MitkoBachvarov <m.bachvarov@tekaris.com>
2019-01-21 18:36:57 +0100

    Adding paths to Calendar and About

--- ProductSite/src/app/app.routing.ts
+++ ProductSite/src/app/app.routing.ts
@@ -1,9 +1,11 @@
+import { AboutPageComponent } from './about-page/about-page.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
 const routes: Routes = [
-    { path: 'products', component: CalendarComponent }
+    { path: 'products', component: CalendarComponent },
+    { path: 'about', component: AboutPageComponent }
 ];
 
 export const routingModule: ModuleWithProviders = RouterModule.forRoot(routes);
